ebfd7fcfd76ea13c4258aaf7e97fc4e3ac23d35b
Hector Coto <hcoto@nabancard.com>
2017-07-26 12:29:04 -0600

    This is also needed for the new react-router to work properly

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import { routerReducer as routing } from 'react-router-redux';
 
 export default combineReducers({
-	state: (state = {}) => state
+	state: (state = {}) => state,
+	routing,
 });

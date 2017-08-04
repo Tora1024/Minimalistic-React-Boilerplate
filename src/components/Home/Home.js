eec44e4237596f4f4740e660c04720f5eb457985
Tora1024 <heavenhell64@gmail.com>
2017-08-04 16:26:52 -0600

    Not a layout anymore

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
-import PropTypes from 'prop-types';
 import styles from './home.css';
 
 class Home extends Component {
   render () {
     return (
       <div styleName='container'>
-        
-        { this.props.children }
-        home
+        Home
       </div>
 	);
   }
-}
-
-Home.propTypes = {
-  children: PropTypes.object,
 }
 
 export default CSSModules(Home, styles);

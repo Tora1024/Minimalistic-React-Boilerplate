50abfb5667ee87190c5f113f8281dd66c0b5f3a9
Tora1024 <heavenhell64@gmail.com>
2017-08-08 16:09:32 -0600

    Adjustments in order to use the new layout

--- src/router.js
+++ src/router.js
@@ -6,8 +6,7 @@ import {
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import Header from './components/Commons/Header/Header';
-
+import Layout from './components/Common/Layout/Layout';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
@@ -15,16 +14,11 @@ import NotFound from './components/NotFound/NotFound';
 const Routes = ({ history }) => {
   return (
     <Router history={history} >
-      <main>
-        <Header />
-
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route component={NotFound} />
-        </Switch>
-        
-      </main>
+      <Switch>
+        <Layout exact path='/' component={Home} />
+        <Layout path='/about' component={About} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 };

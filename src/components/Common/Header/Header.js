ba0629e701dcf92e95579d09b960a24aec766d06
Tora1024 <heavenhell64@gmail.com>
2017-08-09 10:56:46 -0600

    Changing markup of header a bit. Modifying the nav in a simpler more concise way, and adding css modules to handle styles

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CSSModules from 'react-css-modules';
+import styles from './header.css';
 
 const Header = () => {
   return (
-    <header>	
-      <h1>Minimalistic Boilerplate</h1>
-      <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-        </ul>
+    <header styleName='header'>	
+      <h1 styleName='title'>Minimalistic Boilerplate</h1>
+      <nav styleName='nav'>
+        <Link styleName='nav-item' to='/'>Home</Link>
+        <Link styleName='nav-item' to='/about'>About</Link>
       </nav>
     </header>
 	);
 }
 
-export default Header;
+export default CSSModules(Header, styles);

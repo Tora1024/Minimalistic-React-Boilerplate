8f7f8de2fc38e349a74c6481facce43309dbaef6
Tora1024 <heavenhell64@gmail.com>
2017-08-09 10:55:33 -0600

    Changing the markup of the footer component, from class to functional one, also modifying the way navigation markup was handled, and adding actual links to it. Also arranging css modules to the mix

--- src/components/Common/Footer/Footer.js
+++ src/components/Common/Footer/Footer.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CSSModules from 'react-css-modules';
+import styles from './footer.css';
 
-class Footer extends Component {
-  render () {
-    return (
-      <header>
-        <nav>
-          <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+const Footer = () => {
+  return (
+    <footer styleName='footer'>
+      <nav styleName='nav'>
+        <Link styleName='nav-item' to='/'>Home</Link>
+        <Link styleName='nav-item' to='/about'>About</Link>
+      </nav>
+      <div>Social Media</div>
+    </footer>
+  );
 }
 
-export default Footer;
+export default CSSModules(Footer, styles);

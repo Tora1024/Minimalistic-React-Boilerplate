e59cda2283581565b4d5252a6fe9c6b0c5a3073a
Tora1024 <heavenhell64@gmail.com>
2017-08-10 19:05:41 -0600

    Adding “replace” on links in order to avoid warnings

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -6,10 +6,10 @@ import styles from './header.css';
 const Header = () => {
   return (
     <header styleName='header'>	
-      <h1 styleName='title'>Minimalistic Boilerplate</h1>
+      <h1 styleName='title'>Minimalistic React Boilerplate</h1>
       <nav styleName='nav'>
-        <Link styleName='nav-item' to='/'>Home</Link>
-        <Link styleName='nav-item' to='/about'>About</Link>
+        <Link styleName='nav-item' to='/' replace>Home</Link>
+        <Link styleName='nav-item' to='/about' replace>About</Link>
       </nav>
     </header>
 	);

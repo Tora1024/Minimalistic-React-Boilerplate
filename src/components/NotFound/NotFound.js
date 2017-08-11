13e5b9a3329a6f7eb352a02ba1e5eaa0e0016db9
Tora1024 <heavenhell64@gmail.com>
2017-08-10 19:03:08 -0600

    Changing markup of notFound component, adding css modules

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CSSModules from 'react-css-modules';
+import styles from './notFound.css';
 
 const NotFound = () => {
   return (
-    <div className="container">
-      404 not found!
-    </div>
+    <main styleName='container'>
+			<section styleName='inner-container'>
+				<div styleName='lost'></div>
+				<h1 styleName='headline'>Uhh ohh! Not sure what happened, but you are out of reach right now..</h1>
+				<Link styleName='go-back' to='/'>Please, take me back home!</Link>
+			</section>
+    </main>
   );
 };
 
-export default NotFound;
+export default CSSModules(NotFound, styles);
